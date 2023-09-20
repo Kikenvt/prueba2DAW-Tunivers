7ba1ba25107f140802191fa8e918428e76a2f5a1
Enrique Fdez-Campoamor <enriquefernandez-campoamorfernandez@alumnos.tuniversformacion.es>
2023-09-20 09:12:35 +0200

    modificado boton, explicacion de clase

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+/* Lo que busque por internet
+
 let boton=document.querySelector("#cambiar_color");
 boton.addEventListener("click", ()=>
     {
@@ -7,3 +9,20 @@ boton.addEventListener("click", ()=>
         let aleatorio4= Math.floor(Math.random() * (255+1-0) + 0);
         document.body.style.backgroundColor=`rgba(${aleatorio1}, ${aleatorio2}, ${aleatorio3}, ${aleatorio4})`;
     });
+*/
+
+// Funcion para generar un color aleatorio en formato hexadecimal
+const generateRandomColor = () => {
+  const color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  console.log("Color hexadecimal generado: " + color);
+  return color;
+};
+
+// Funcionpara cambiar el color de fondo del body
+const changeBackgroundColor = () => {
+  const newColor = generateRandomColor();
+  document.body.style.backgroundColor = newColor;
+};
+
+//Agregar un evento al boton para cambiar el color al hacer click
+const changeColorBtn = document.getElementById("changeColorBtn");
